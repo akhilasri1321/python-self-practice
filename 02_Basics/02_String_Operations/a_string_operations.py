86c7ad97c075ebf452882c0d30033eb18f534043
akhilasri1321 <97819798+akhilasri1321@users.noreply.github.com>
2025-01-21 23:14:17 +0000

    string Operations

---
+++ 02_Basics/02_String_Operations/a_string_operations.py
@@ -0,0 +1,31 @@
+#!usr/bin/python3
+
+"""
+purpose : string Operations
+     strings can be represented using:
+        1.) Single quotes,
+        2.) double quotes or
+        3.) triple quotes
+        4.)Triple double quotes
+
+    pep8 --> use triple double quotes for doc - strings
+
+"""
+
+# String with single quotes
+language = 'Python Programming'
+print(language, type(language))
+
+# String with double quotes
+language = "Python Programming"
+print(language, type(language))
+
+
+# String with triple single quotes
+language = '''Python Programming'''
+print(language, type(language))
+
+
+# String with triple double quotes
+language = """Python Programming"""
+print(language, type(language))
